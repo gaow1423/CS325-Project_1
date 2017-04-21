b7e9fb13b1e0620135742b32c2b929603ce69cba
inbergt <inbergt@oregonstate.edu>
2017-04-20 20:20:34 -0700

    Calculates the value

--- project_1_alg_3.cpp
+++ project_1_alg_3.cpp
@@ -13,17 +13,16 @@
 
 using namespace std;
 
-int MaxArray (int array[], int beginning, int end);
+int MaxArray (int* array, int beginning, int end);
 int MaxSuffix (int array[], int beginning, int end);
 int MaxPrefix (int array[], int beginning, int end);
 
 int main () {	
 	int beginning = 0;
-	int end = 10;
+	int end = 9;
 	int array [10] = {31,-41,59,26,-53,58,97,-93,-23,84};
-	int solution;
 
-	solution = MaxArray(array,beginning,end);
+	int solution = MaxArray(array,beginning,end);
 	
     cout << "The max array is " << solution << "!\n";
    
@@ -35,15 +34,16 @@ int MaxArray (int array[], int beginning, int end){
    if (beginning == end)
 	   return array[beginning];
    
-    int middle = (end-beginning)/2;
+    int middle = (end+beginning)/2;
 	
 	int left_max_array = MaxArray(array, beginning, middle);
-	int right_max_array = MaxArray(array, middle+1,end);
+	int right_max_array = MaxArray(array, middle+1, end);
 	int middle_max_array = MaxSuffix(array,beginning, middle) + MaxPrefix(array,middle+1,end);
-	
+
+	//cout << middle_max_array << " middle \n";	
 	if (left_max_array < right_max_array)
 	   left_max_array = right_max_array;
-    else if (left_max_array < middle_max_array)
+    if (left_max_array < middle_max_array)
    	   left_max_array = middle_max_array;
 	return left_max_array;	
 }
@@ -52,13 +52,12 @@ int MaxSuffix (int array[], int beginning, int end){
     int max = 0;
 	int sum = 0;
 
-   if (beginning == end)
-	   return array[beginning];
-	for (int i = end; i>beginning-1; i--){
+	for (int i = end; i>=beginning; i--){
 		sum = sum + array[i];
 		if (sum>max)
 	    max = sum;
 		}
+	//cout << max << " suffix \n";
 
 	return max;	
 }
@@ -67,12 +66,11 @@ int MaxPrefix (int array[], int beginning, int end){
     int max = 0;
 	int sum = 0;
 
-   if (beginning == end)
-	   return array[beginning];
-	for (int i = beginning; i<end+1; i++){
+	for (int i = beginning; i<=end; i++){
 		sum = sum + array[i];
 		if (sum>max)
 	    max = sum;
 		}
+	//cout << max << " prefix \n";
 	return max; 
 }
