63c80fe2c063f3ce855fffe9bb1c33de6bb2854c
Wei Gao <gaow@flip3.engr.oregonstate.edu>
2017-04-21 16:40:35 -0700

    ...

--- project_1_alg_4.cpp
+++ project_1_alg_4.cpp
@@ -12,24 +12,24 @@ int main(){
        	int array [10] = {31,-41,59,26,-53,58,97,-93,-23,84};
 	int sum;
 	int max = array[0];
-	int high=0, low=0;
-	int ehigh=0, elow=0;
+//	int high=0, low=0;
+//	int ehigh=0, elow=0;
 	sum = array[0];
 
 	for(int i=1; i<10; i++){
-		ehigh = i;
+	//	ehigh = i;
 		if(sum > 0){
 			sum += array[i];
 		}
 		else{
-			elow = i;
+		//	elow = i;
 			sum = array[i];
 		}
 		
 		if(sum > max){
 			max = sum;
-			low = elow;
-			high = ehigh;
+		//	low = elow;
+		//	high = ehigh;
 		}
 	}
 	cout << "Max is " << max << endl;
